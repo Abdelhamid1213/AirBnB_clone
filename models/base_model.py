06ac4a5b2ba5e0cb1f3ee4df575c215d89745180
Abdelhamid <abdelhamidelmaazi@gmail.com>
2023-12-10 08:59:36 +0100

    Tasks

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Base module for airbnb clone"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """
+        Constructor for BaseModel class.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """
+        Updates the public instance attribute 'updated_at' with the
+        current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance.
+        """
+        result_dict = self.__dict__.copy()
+        result_dict['__class__'] = self.__class__.__name__
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
+        return result_dict
+
+    def __str__(self):
+        """
+        Returns a string representation of the instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
